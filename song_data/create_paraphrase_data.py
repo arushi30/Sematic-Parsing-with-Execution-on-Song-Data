00d7e1c877cdf48e499da447c5bca9d49ab13d98
brinaseidel <brina.seidel@gmail.com>
2019-05-05 23:12:27 -0400

    Changing train/val/test split method

--- song_data/create_paraphrase_data.py
+++ song_data/create_paraphrase_data.py
@@ -6,33 +6,30 @@ Processes google spreadsheet of paraphrases to create training, validation, and 
 import inspect
 import os
 import pandas as pd
-import numpy as np
 import csv
+from sklearn.model_selection import GroupShuffleSplit
 
 filename = inspect.getframeinfo(inspect.currentframe()).filename
 path = os.path.dirname(os.path.abspath(filename))
 
-np.random.seed(5)
-
 # Read in the google sheet
 google_sheet = "https://docs.google.com/spreadsheets/d/1StM4qy-hG4FETenJirooV6IT8SN6Y1RAkYWGC3e8r0M/export?format=csv"
 paraphrases = pd.read_csv(google_sheet)
+paraphrases.dropna(inplace=True)
+paraphrases.reset_index(inplace=True)
 
 # Create a single column with the format we need to write out
 paraphrases["final"] = "(example\n  (utterance \"" + paraphrases["paraphrase"] + "\")\n  (original \"" + paraphrases["canonical_utterance"]+"\")\n  (targetFormula \n    " + paraphrases["do_not_edit"] + "\n  )\n)"
+paraphrases=paraphrases.apply(lambda x: x.str.strip() if x.dtype == "object" else x)
 
-# Split into train, validation, and test data
-paraphrases["random"]=np.random.rand(len(paraphrases))
-paraphrases=paraphrases.sort_values("random").reset_index(drop=True)
-test_size = len(paraphrases)/5
-train = paraphrases.loc[0:3*test_size-1, "final"]
-val = paraphrases.loc[3*test_size:4*test_size-1, "final"]
-test = paraphrases.loc[4*test_size:, "final"]
+# Split into train and test
+train_inds, test_inds = next(GroupShuffleSplit(test_size=.20, n_splits=2, random_state = 7).split(paraphrases, groups=paraphrases['canonical_utterance']))
+train, test = paraphrases.loc[train_inds, :], paraphrases.loc[test_inds, 'final']
 
-# Drop missing and clean whitespace
-for df in [train, val, test]:
-    df.dropna(inplace=True)
-    df.str.strip()
+# Split training data into train and validation
+train_inds, val_inds = next(GroupShuffleSplit(test_size=.25, n_splits=2, random_state = 7).split(train, groups=train['canonical_utterance']))
+train, val = paraphrases.loc[train_inds, 'final'], paraphrases.loc[val_inds, 'final']
+
     
 # Write to appropriate files
 pd.set_option("display.max_colwidth", 10000)
