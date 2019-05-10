50677d27719b64d5d2ce3ccb3f953fcd419a91cb
mmarinated <marina.a.zavalina@gmail.com>
2019-05-10 13:54:35 -0400

    paralleled training script

--- songs_script.py
+++ songs_script.py
@@ -5,6 +5,8 @@
 # Make sure there are ppdb files.
 
 import subprocess
+from joblib import Parallel, delayed
+from itertools import product
 
 def execute_command(command, stdout, cwd=None):
     if cwd:
@@ -16,40 +18,49 @@ def execute_command(command, stdout, cwd=None):
     return 0
 
 
-for num in [50, 75, 100, 125, 150]:
-    for version in range(0, 10):
-        # paths to train, test
-        train_examples = "lib/data/overnight/songs.paraphrases.train_size_"+str(num)+"_v"+str(version)+".examples"
-        test_examples = 'lib/data/overnight/songs.paraphrases.test.examples'
-        ppdb = "lib/data/overnight/train_size_"+str(num)+"_v"+str(version)+"_ppdb.txt"
-        # path to logfile
-        logfile = 'logs/logfile_size_'+str(num)+'_v'+str(version)+'.txt'
-        print('\n' + logfile + '\n')
+def f(num, version):
+    # paths to train, test
+    train_examples = "lib/data/overnight/songs.paraphrases.train_size_"+str(num)+"_v"+str(version)+".examples"
+    test_examples = 'lib/data/overnight/songs.paraphrases.test.examples'
+    ppdb = "lib/data/overnight/train_size_"+str(num)+"_v"+str(version)+"_ppdb.txt"
+    # path to logfile
+    logfile = 'logs/logfile_size_'+str(num)+'_v'+str(version)+'.txt'
+    print('\n' + logfile + '\n')
 
-        with open(logfile, 'w') as f:
-            ## Berkeley Aligner
-            # generate .e and .f files
-            command = './run @mode=berkeley --BerkeleyInputMain.input {}'.format(train_examples)
-            execute_command(command, f)
-            print('.e and .f files are generated.')
+    with open(logfile, 'w') as f:
+        ## Berkeley Aligner
+        # generate .e and .f files
+        command = './run @mode=berkeley --BerkeleyInputMain.input {}'.format(train_examples)
+        execute_command(command, f)
+        print('.e and .f files are generated.')
 
-            # generate the relevant files using the berkeley aligner
-            execute_command('bash ./berkeley', f, 'berkeleyaligner/')
-            print('Berkeleyaligner finished.')
+        # generate the relevant files using the berkeley aligner
+        execute_command('bash ./berkeley', f, 'berkeleyaligner/')
+        print('Berkeleyaligner finished.')
 
-            # generate the word_alignment file that has the actual features used by SEMPRE
-            command = './run @mode=aligner --AlignerMain.input lib/data/overnight/songs.word_alignments.berkeley berkeley 2'
-            execute_command(command, f)
-            print('word_alignment file is generated.')
+        # generate the word_alignment file that has the actual features used by SEMPRE
+        command = './run @mode=aligner --AlignerMain.input lib/data/overnight/songs.word_alignments.berkeley berkeley 2'
+        execute_command(command, f)
+        print('word_alignment file is generated.')
 
-            ## Training the model
-            params_dict = {
-                'mode': 'songs', 
-                'Dataset': ('train:' + train_examples + ' ' + 'test:' + test_examples),
-                'ppdb_path': ppdb,
-            }
-            command = './run @mode={mode} -Dataset.inPaths {Dataset} -PPDBModel.ppdbModelPath {ppdb_path}'.format(**params_dict)
-            print('\n' + command + '\n')
-            execute_command(command, f)
+        ## Training the model
+        params_dict = {
+            'mode': 'songs', 
+            'Dataset': ('train:' + train_examples + ' ' + 'test:' + test_examples),
+            'ppdb_path': ppdb,
+        }
+        command = './run @mode={mode} -Dataset.inPaths {Dataset} -PPDBModel.ppdbModelPath {ppdb_path}'.format(**params_dict)
+        print('\n' + command + '\n')
+        execute_command(command, f)
+            
+            
+num_arr = range(125, 151, 25)
+version_arr =  range(0, 10)
+
+Parallel(n_jobs=4)(
+    delayed(f)(num, version)
+    for (num, version) in product(num_arr,
+                                  version_arr)
+)
 
 print('end')
